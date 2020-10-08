81f484cb0d0acbecc280e9a58f765c7114cc8d90
Omar Benchbana <omar.bb@hotmail.fi>
2020-10-08 10:16:58 +0300

    added delete

--- routes/api.js
+++ routes/api.js
@@ -16,7 +16,7 @@ const Issues = require('./../models/Issues');
 
 const CONNECTION_STRING = process.env.DB;
 
-mongoose.connect(CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify: true});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -52,12 +52,40 @@ module.exports = function (app) {
 
     .put(function (req, res){
       var project = req.params.project;
+      if (req.body['_id']&& req.body.length > 1) {
+Issues.findByIdAndUpdate(req.body["_id"],{issue_title:req.body.issue_title,issue_text:req.body.issue_text,created_by:req.body.created_by}
+, function(err, result){
 
+        if(err){
+            res.send('could not update '+req.body["_id"])
+        }
+        else{
+            res.send("successfully updated")
+        }
+
+    })
+
+      }
+      else {
+        res.send("no updated field sent")
+      }
+console.log(req.body)
     })
 
     .delete(function (req, res){
       var project = req.params.project;
-
+      if (req.body['_id']) {
+        Model.remove({ _id: req.body['_id'] }, function(err) {
+            if (!err) {
+                    res.send('deleted '+ req.body['_id'])
+            }
+            else {
+              res.send('could not delete  '+ req.body['_id'])
+            }
+        });
+      }else {
+        res.send('id error')
+      }
     });
 
 };
